77cfdd9a506a65691f3c09ecd8a7df62478f3f80
caioalmeida12 <araujo.caoi08@aluno.ifce.edu.br>
2024-07-14 11:16:18 -0300

    implemented advent of code 2023 task 3

--- advent_of_code_2023_3/lib/advent_of_code20233.ex
+++ advent_of_code_2023_3/lib/advent_of_code20233.ex
@@ -1,10 +1,8 @@
 defmodule AdventOfCode20233 do
   @string_delimiter "\r\n"
 
-  @type file_content :: list(String.t())
-  @type outer_matrix :: list(list(String.t()))
-  @type symbol_position :: %{line: integer(), col: integer()}
-  @type inner_matrix :: {outer_matrix(), symbol_position()}
+  @type line :: String.t()
+  @type file_content :: list(line())
 
   @spec read_file(String.t()) :: file_content()
   def read_file(path) do
@@ -14,196 +12,214 @@ defmodule AdventOfCode20233 do
     end
   end
 
-  @spec get_all_symbol_positions(file_content()) :: list(symbol_position())
-  def get_all_symbol_positions(file_content) do
+  defp convert_list_of_integers_to_list_of_ranges(line) when length(line) == 0, do: []
+
+  defp convert_list_of_integers_to_list_of_ranges(line) when is_list(line) and length(line) > 0 do
+    current_val = Enum.at(line, 0)
+    next_val = Enum.at(line, 1)
+
+    first_continuous_ind = 0
+    current_ind = 0
+
+    if current_val == next_val - 1 do
+      # is continuous
+      convert_list_of_integers_to_list_of_ranges(
+        line,
+        first_continuous_ind,
+        current_ind + 1,
+        current_ind,
+        []
+      )
+    else
+      # its not continuous
+      convert_list_of_integers_to_list_of_ranges(
+        line,
+        first_continuous_ind,
+        current_ind + 1,
+        current_ind,
+        [0..0]
+      )
+    end
+  end
+
+  defp convert_list_of_integers_to_list_of_ranges(
+         line,
+         first_continuous_ind,
+         _current_ind,
+         last_continuous_ind,
+         acc
+       )
+       when last_continuous_ind == length(line) - 1,
+       do:
+         acc ++
+           [Range.new(Enum.at(line, first_continuous_ind), Enum.at(line, last_continuous_ind))]
+
+  defp convert_list_of_integers_to_list_of_ranges(
+         line,
+         first_continuous_ind,
+         current_ind,
+         _last_continuous_ind,
+         acc
+       ) do
+    current_val = Enum.at(line, current_ind)
+    next_val = Enum.at(line, current_ind + 1)
+
+    if next_val != nil and current_val == next_val - 1 do
+      # is continuous
+      convert_list_of_integers_to_list_of_ranges(
+        line,
+        first_continuous_ind,
+        current_ind + 1,
+        current_ind + 1,
+        acc
+      )
+    else
+      # its not continuous
+      new_acc =
+        if Enum.at(line, first_continuous_ind) == nil and Enum.at(line, current_ind) == nil,
+          do: acc,
+          else:
+            acc ++ [Range.new(Enum.at(line, first_continuous_ind), Enum.at(line, current_ind))]
+
+      if current_ind <= length(line) do
+        convert_list_of_integers_to_list_of_ranges(
+          line,
+          current_ind + 1,
+          current_ind + 1,
+          current_ind + 1,
+          new_acc
+        )
+      else
+        acc
+      end
+    end
+  end
+
+  @spec get_number_coordinates_for_line(file_content(), integer()) :: list(tuple())
+  def get_number_coordinates_for_line(file_content, line_index) do
+    line =
+      file_content
+      |> Enum.at(line_index)
+
+    list_of_integer_coordinates =
+      line
+      |> String.graphemes()
+      |> Enum.with_index()
+      |> Enum.filter(fn {char, _} -> String.match?(char, ~r/\d/) end)
+      |> Enum.map(fn {_char, coord} -> coord end)
+
+    ranges = convert_list_of_integers_to_list_of_ranges(list_of_integer_coordinates)
+
+    list_of_coordinates =
+      ranges
+      |> Enum.map(fn range ->
+        Enum.map(range, fn col -> {line_index, col} end)
+      end)
+
+    list_of_coordinates
+    |> Enum.map(fn adjacent_coordinates ->
+      # adjacent_coordinates = [{0, 0}, {0, 1}, {0, 2}]
+
+      # [0, 1, 2]
+      cols_with_number =
+        adjacent_coordinates
+        |> Enum.map(fn {_, col} -> col end)
+
+      list_of_chars =
+        line
+        |> String.graphemes()
+
+      number =
+        cols_with_number
+        |> Enum.map(&Enum.at(list_of_chars, &1))
+        |> Enum.join()
+        |> String.to_integer()
+
+      # {467, [{0, 0}, {0, 1}, {0, 2}]}
+      {number, adjacent_coordinates}
+    end)
+  end
+
+  @spec get_symbol_coordinates(file_content()) :: list(tuple())
+  def get_symbol_coordinates(file_content) do
     file_content
     |> Enum.with_index()
     |> Enum.flat_map(fn {line, line_ind} ->
-      line
+      graphemes = String.graphemes(line)
+
+      graphemes
       |> Enum.with_index()
-      |> Enum.map(fn {col, col_ind} ->
-        if String.match?(col, ~r/[^0-9.]/), do: %{line: line_ind, col: col_ind}, else: nil
+      |> Enum.map(fn {_col, col_ind} ->
+        value =
+          graphemes
+          |> Enum.at(col_ind)
+
+        unless String.match?(value, ~r/^\d$|^\.$/) do
+          {line_ind, col_ind}
+        end
       end)
     end)
-    |> Enum.filter(&(&1 != nil))
+    |> Enum.filter(& &1)
   end
 
-  @spec get_imediate_neighbours(outer_matrix(), symbol_position()) :: outer_matrix()
-  def get_imediate_neighbours(matrix, %{line: 0, col: 0}) do
-    above = [".", ".", "."]
-    middle = [".", ".", matrix |> Enum.at(0) |> Enum.at(1)]
-    below = [".", matrix |> Enum.at(1) |> Enum.at(0), matrix |> Enum.at(1) |> Enum.at(1)]
+  @spec get_adjacent_coordinates_of_symbol(file_content(), tuple()) :: list(tuple())
+  def get_adjacent_coordinates_of_symbol(file_content, {line, col}) do
+    max_line = file_content |> Enum.count()
+    max_col = file_content |> Enum.at(0) |> String.graphemes() |> Enum.count()
 
-    {[above, middle, below], %{line: 0, col: 0}}
-  end
+    min_line = 0
+    min_col = 0
 
-  def get_imediate_neighbours(matrix, %{line: 0, col: 1}) do
-    above = [".", ".", "."]
-    middle = [matrix |> Enum.at(0) |> Enum.at(0), ".", matrix |> Enum.at(0) |> Enum.at(2)]
+    possible_lines = [max(min_line, line - 1), line, min(max_line, line + 1)] |> Enum.uniq()
+    possible_cols = [max(min_col, col - 1), col, min(max_col, col + 1)] |> Enum.uniq()
 
-    below = [
-      matrix |> Enum.at(1) |> Enum.at(0),
-      matrix |> Enum.at(1) |> Enum.at(1),
-      matrix |> Enum.at(1) |> Enum.at(2)
-    ]
+    possible_coords =
+      possible_lines
+      |> Enum.flat_map(fn line_ind ->
+        possible_cols
+        |> Enum.map(&{line_ind, &1})
+      end)
 
-    {[above, middle, below], %{line: 0, col: 1}}
-  end
-
-  def get_imediate_neighbours(matrix, %{line: 1, col: 0}) do
-    above = [".", matrix |> Enum.at(0) |> Enum.at(0), matrix |> Enum.at(0) |> Enum.at(1)]
-    middle = [".", ".", matrix |> Enum.at(1) |> Enum.at(1)]
-    below = [".", matrix |> Enum.at(2) |> Enum.at(0), matrix |> Enum.at(2) |> Enum.at(1)]
-
-    {[above, middle, below], %{line: 1, col: 0}}
-  end
-
-  def get_imediate_neighbours(matrix, %{line: line, col: 0}) when line == length(matrix) - 1 do
-    above = [
-      ".",
-      matrix |> Enum.at(line - 1) |> Enum.at(0),
-      matrix |> Enum.at(line - 1) |> Enum.at(1)
-    ]
-
-    middle = [".", ".", matrix |> Enum.at(line) |> Enum.at(1)]
-    below = [".", ".", "."]
-
-    {[above, middle, below], %{line: line, col: 0}}
-  end
-
-  def get_imediate_neighbours(matrix, %{line: line, col: 1}) when line == length(matrix) - 1 do
-    above = [
-      matrix |> Enum.at(line - 1) |> Enum.at(0),
-      matrix |> Enum.at(line - 1) |> Enum.at(1),
-      matrix |> Enum.at(line - 1) |> Enum.at(2)
-    ]
-
-    middle = [matrix |> Enum.at(line) |> Enum.at(0), ".", matrix |> Enum.at(line) |> Enum.at(2)]
-    below = [".", ".", "."]
-
-    {[above, middle, below], %{line: line, col: 1}}
-  end
-
-  def get_imediate_neighbours(matrix, %{line: line, col: col}) when line == length(matrix) - 1 do
-    # check if it's the last column
-    max_col_ind = length(Enum.at(matrix, 0)) - 1
-
-    if col == max_col_ind do
-      above = [
-        matrix |> Enum.at(line - 1) |> Enum.at(col - 1),
-        matrix |> Enum.at(line - 1) |> Enum.at(col),
-        "."
-      ]
-
-      middle = [matrix |> Enum.at(line) |> Enum.at(col - 1), ".", "."]
-      below = [".", ".", "."]
-
-      {[above, middle, below], %{line: line, col: col}}
-    else
-      # Handle cells on the last row but not in the last column
-      above = [
-        matrix |> Enum.at(line - 1) |> Enum.at(col - 1),
-        matrix |> Enum.at(line - 1) |> Enum.at(col),
-        matrix |> Enum.at(line - 1) |> Enum.at(col + 1)
-      ]
-
-      middle = [
-        matrix |> Enum.at(line) |> Enum.at(col - 1),
-        ".",
-        matrix |> Enum.at(line) |> Enum.at(col + 1)
-      ]
-
-      below = [".", ".", "."]
-
-      {[above, middle, below], %{line: line, col: col}}
-    end
-  end
-
-  def get_imediate_neighbours(matrix, %{line: line, col: col}) do
-    above = [
-      matrix |> Enum.at(line - 1) |> Enum.at(col - 1),
-      matrix |> Enum.at(line - 1) |> Enum.at(col),
-      matrix |> Enum.at(line - 1) |> Enum.at(col + 1)
-    ]
-
-    middle = [
-      matrix |> Enum.at(line) |> Enum.at(col - 1),
-      ".",
-      matrix |> Enum.at(line) |> Enum.at(col + 1)
-    ]
-
-    below = [
-      matrix |> Enum.at(line + 1) |> Enum.at(col - 1),
-      matrix |> Enum.at(line + 1) |> Enum.at(col),
-      matrix |> Enum.at(line + 1) |> Enum.at(col + 1)
-    ]
-
-    {[above, middle, below], %{line: line, col: col}}
-  end
-
-  def is_left_a_number(line, index) do
-    is_left_a_number(line, index, [])
-  end
-
-  defp is_left_a_number(_line, 0, acc), do: acc
-
-  defp is_left_a_number(line, index, acc) do
-    char = Enum.at(line, index - 1)
-
-    if String.match?(char, ~r/\d/) do
-      # If the character is a number, add it to the accumulator and continue left
-      is_left_a_number(line, index - 1, [char | acc])
-    else
-      # If the character is not a number, return the accumulated numbers
-      acc
-    end
-  end
-
-  @spec get_numbers_to_the_left(outer_matrix(), inner_matrix()) :: integer()
-  def get_numbers_to_the_left(
-        outer_matrix,
-        {inner_matrix, %{line: _inner_line, col: inner_col}}
-      ) do
-    outer_matrix
-    |> Enum.with_index()
-    |> Enum.map(fn {outer_line_value, outer_line_index} ->
-      last_number =
-        inner_matrix
-        |> Enum.at(outer_line_index)
-        |> Enum.with_index()
-        |> Enum.reverse()
-        |> Enum.find(&String.match?(Tuple.to_list(&1) |> Enum.at(0), ~r/\d/))
-
-      if (last_number != nil) do
-        # take a line of the inner matrix and get the index of the last number
-        {last_number, last_number_index} =
-          inner_matrix
-          |> Enum.at(outer_line_index)
-          |> IO.inspect()
-          |> Enum.with_index()
-          |> Enum.reverse()
-          |> Enum.find(&String.match?(Tuple.to_list(&1) |> Enum.at(0), ~r/\d/))
-
-        # extrapolate the last_number_index to the scope of the outer matrix
-        outer_matrix_last_number_index = inner_col - 1 + last_number_index
-
-        # check if the digit to the left of the outer_matrix_last_number_index is a number
-
-        numbers_left =
-          is_left_a_number(
-            outer_line_value,
-            outer_matrix_last_number_index
-          )
-
-        # returns the full number
-        [numbers_left | [last_number]]
-        |> List.flatten()
-        |> Enum.join()
-        |> String.to_integer()
-      else
-        # no numbers in the line
-        0
-      end
-    end)
+    # reject the symbol position, as it is never gonna be a number
+    possible_coords
+    |> Enum.reject(&(&1 == {line, col}))
   end
 end
+
+file_content = AdventOfCode20233.read_file("./lib/input.txt")
+
+numbers_with_coordinates =
+  file_content
+  |> Enum.with_index()
+  |> Enum.flat_map(fn {_line, line_ind} ->
+    AdventOfCode20233.get_number_coordinates_for_line(file_content, line_ind)
+  end)
+
+symbol_adjacent_coordinates =
+  file_content
+  |> AdventOfCode20233.get_symbol_coordinates()
+  |> Enum.map(&AdventOfCode20233.get_adjacent_coordinates_of_symbol(file_content, &1))
+
+symbol_adjacent_coordinates
+|> Enum.flat_map(fn coords_of_line ->
+  coords_of_line
+  |> Enum.map(fn neighbour_of_symbol ->
+    numbers_with_coordinates
+    |> Enum.map(fn {number, coordinates} ->
+      if Enum.find_index(coordinates, &(neighbour_of_symbol == &1)), do: number, else: 0
+    end)
+
+    # for every coordinate adjacent of a symbol (neighbour_of_symbol)
+    # take the numbers_with_coordinates
+    # for every number
+    # take the coordinates of it
+    # check if this neighbour_of_symbol is in that enum
+    # if it is, return the number
+    # else, return 0
+  end)
+end)
+|> Enum.sort()
+|> Enum.dedup()
+|> List.flatten()
+|> Enum.sum()
+|> IO.inspect()
--- advent_of_code_2023_3/test/advent_of_code20233_test.exs
+++ advent_of_code_2023_3/test/advent_of_code20233_test.exs
@@ -2,73 +2,27 @@ defmodule AdventOfCode20233Test do
   use ExUnit.Case
   doctest AdventOfCode20233
 
-  @test_matrix [
-    ["4", "6", "7", ".", "."],
-    [".", ".", "*", ".", "."],
-    [".", ".", "3", "5", "3"]
-  ]
-
   test "reads the file" do
-    assert AdventOfCode20233.read_file("./lib/input.txt")
+    assert AdventOfCode20233.read_file("./lib/example_input.txt")
   end
 
-  test "detects a symbol and outputs its position" do
-    assert @test_matrix |> AdventOfCode20233.get_all_symbol_positions() == [%{line: 1, col: 2}]
+  test "gets the tuple of {number, adjacent_coordinates} for every number in a line" do
+    assert AdventOfCode20233.read_file("./lib/example_input.txt")
+           |> AdventOfCode20233.get_number_coordinates_for_line(0) == [
+             {467, [{0, 0}, {0, 1}, {0, 2}]},
+             {114, [{0, 5}, {0, 6}, {0, 7}]}
+           ]
   end
 
-  test "gets all imediate neighbours of a symbol" do
-    assert @test_matrix |> AdventOfCode20233.get_imediate_neighbours(%{line: 0, col: 0}) ==
-             {[
-                [".", ".", "."],
-                [".", ".", "6"],
-                [".", ".", "."]
-              ], %{line: 0, col: 0}}
-
-    assert @test_matrix |> AdventOfCode20233.get_imediate_neighbours(%{line: 0, col: 1}) ==
-             {[
-                [".", ".", "."],
-                ["4", ".", "7"],
-                [".", ".", "*"]
-              ], %{line: 0, col: 1}}
-
-    assert @test_matrix |> AdventOfCode20233.get_imediate_neighbours(%{line: 1, col: 0}) ==
-             {[
-                [".", "4", "6"],
-                [".", ".", "."],
-                [".", ".", "."]
-              ], %{line: 1, col: 0}}
-
-    assert @test_matrix
-           |> AdventOfCode20233.get_imediate_neighbours(%{line: length(@test_matrix) - 1, col: 0}) ==
-             {[[".", ".", "."], [".", ".", "."], [".", ".", "."]],
-              %{line: length(@test_matrix) - 1, col: 0}}
-
-    assert @test_matrix
-           |> AdventOfCode20233.get_imediate_neighbours(%{line: length(@test_matrix) - 1, col: 1}) ==
-             {[[".", ".", "*"], [".", ".", "3"], [".", ".", "."]],
-              %{line: length(@test_matrix) - 1, col: 1}}
-
-    assert @test_matrix
-           |> AdventOfCode20233.get_imediate_neighbours(%{
-             line: length(@test_matrix) - 1,
-             col: length(Enum.at(@test_matrix, 0)) - 1
-           }) ==
-             {[[".", ".", "."], ["5", ".", "."], [".", ".", "."]],
-              %{
-                line: length(@test_matrix) - 1,
-                col: length(Enum.at(@test_matrix, 0)) - 1
-              }}
-
-    assert @test_matrix |> AdventOfCode20233.get_imediate_neighbours(%{line: 1, col: 1}) ==
-             {[["4", "6", "7"], [".", ".", "*"], [".", ".", "3"]], %{line: 1, col: 1}}
-  end
-
-  test "detects all numbers adjacent to a symbol position and outputs their absolute position" do
-    inner_matrix = @test_matrix |> AdventOfCode20233.get_imediate_neighbours(%{line: 1, col: 2})
-
-    assert inner_matrix ==
-             {[["6", "7", "."], [".", ".", "."], [".", "3", "5"]], %{col: 2, line: 1}}
-
-    @test_matrix |> AdventOfCode20233.get_numbers_to_the_left(inner_matrix) |> IO.inspect()
+  test "gets the tuple of coordinates of evey symbol in the file" do
+    assert AdventOfCode20233.read_file("./lib/example_input.txt")
+           |> AdventOfCode20233.get_symbol_coordinates() == [
+             {1, 3},
+             {3, 6},
+             {4, 3},
+             {5, 5},
+             {8, 3},
+             {8, 5}
+           ]
   end
 end
