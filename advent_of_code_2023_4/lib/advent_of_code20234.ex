77274c1662cdbdb371984e730412683c29741a48
caioalmeida12 <araujo.caio08@aluno.ifce.edu.br>
2024-07-16 22:59:45 -0300

    implemented task 2 of aoc 2024 day 4

--- advent_of_code_2023_4/lib/advent_of_code20234.ex
+++ advent_of_code_2023_4/lib/advent_of_code20234.ex
@@ -15,74 +15,87 @@ defmodule AdventOfCode20234 do
   def get_formatted_input(file_content) do
     file_content
     |> Enum.map(fn row ->
-       card_id =
+      card_id =
         row
         |> String.split(":", trim: true)
         |> Enum.at(0)
         |> String.split(" ", trim: true)
         |> Enum.at(1)
 
-        [winning_numbers, card_numbers] =
-          row
-          |> String.split(":", trim: true)
-          |> Enum.at(1)
-          |> String.split("|", trim: true)
-          |> Enum.map(fn side ->
-            side
-            |> String.split(" ")
-            |> Enum.reject(& &1=="")
-          end)
+      [winning_numbers, card_numbers] =
+        row
+        |> String.split(":", trim: true)
+        |> Enum.at(1)
+        |> String.split("|", trim: true)
+        |> Enum.map(fn side ->
+          side
+          |> String.split(" ")
+          |> Enum.reject(&(&1 == ""))
+        end)
 
       %{id: card_id, winning_numbers: winning_numbers, card_numbers: card_numbers}
     end)
   end
 
-  def count_common_numbers(%{id: _card_id, winning_numbers: winning_numbers, card_numbers: card_numbers}) do
+  def count_common_numbers(%{
+        id: _card_id,
+        winning_numbers: winning_numbers,
+        card_numbers: card_numbers
+      }) do
     winning_numbers
     |> Enum.reduce(0, fn number, acc ->
-      if Enum.find(card_numbers, &(number==&1)), do: acc + 1, else: acc
+      if Enum.find(card_numbers, &(number == &1)), do: acc + 1, else: acc
     end)
   end
 
   def get_total_result(list_of_points) do
     list_of_points
     |> Enum.reduce(0, fn val, acc ->
-      if val > 0, do: acc + :math.pow(2, val-1), else: acc
+      if val > 0, do: acc + :math.pow(2, val - 1), else: acc
     end)
   end
 
-  def get_copy_cards(formatted_input, {id, amount}) do
-    index_and_cards =
-      formatted_input
-      |> Enum.with_index()
-      |> Enum.map(fn {_, ind} -> {ind+1, Enum.at(formatted_input, ind)} end)
+  def get_winner_cards(path) do
+    read_file(path)
+    |> Enum.with_index(1)
+    |> Enum.map(fn {line, idx} ->
+      [_, winners, numbers] = String.split(line, ~r/(: )|( \| )/)
 
-    min_range = max((id+1), 0)
-    max_range = min((id+amount), length(formatted_input)-1)
+      winners =
+        winners
+        |> String.split()
+        |> Enum.map(&String.to_integer(&1))
+        |> MapSet.new()
 
-    min_range..max_range
-    |> Enum.map(fn range_id ->
-      index_and_cards
-      |> Enum.find(fn {index, _card} -> range_id == index end)
+      score =
+        numbers
+        |> String.split()
+        |> Enum.map(&String.to_integer(&1))
+        |> Enum.filter(&(&1 in winners))
+        |> length
+
+      {idx, score}
     end)
-    |> Enum.filter(& &1)
-    |> Enum.map( fn {_, card} -> card end)
+  end
+
+  def get_total_scratchcards(winners) do
+    winners
+    |> Enum.reduce(
+      1..length(winners) |> Enum.map(&{&1, 1}) |> Enum.into(%{}),
+      fn {card, wins}, acc ->
+        if wins > 0 do
+          (card + 1)..(card + wins)
+          |> Enum.reduce(acc, fn i, acc -> Map.put(acc, i, acc[i] + acc[card]) end)
+        else
+          acc
+        end
+      end
+    )
+    |> then(&Map.values/1)
+    |> Enum.sum()
   end
 end
 
-# AdventOfCode20234.read_file("./lib/example_input.txt")
-# |> AdventOfCode20234.get_formatted_input()
-# |> Enum.map(&AdventOfCode20234.count_common_numbers/1)
-# |> AdventOfCode20234.get_total_result()
-# |> IO.inspect
-
-formatted_input =
-AdventOfCode20234.read_file("./lib/example_input.txt")
-|> AdventOfCode20234.get_formatted_input()
-
-formatted_input
-|> Enum.map(&AdventOfCode20234.count_common_numbers/1)
-|> Enum.with_index(&({&2+1, &1}))
-|> Enum.map(&AdventOfCode20234.get_copy_cards(formatted_input, &1))
-|> Enum.with_index()
-|> Enum.map(& IO.inspect(&1))
+AdventOfCode20234.get_winner_cards("./lib/input.txt")
+|> AdventOfCode20234.get_total_scratchcards()
+|> IO.inspect()
