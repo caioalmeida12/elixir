6a07f0c030b28c8b6b69b9f921ef8273bfc9ed7f
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-07-24 17:09:24 -0300

    reimplemented aoc2023 day 8 task 1

--- advent_of_code_2023_8/lib/advent_of_code20238.ex
+++ advent_of_code_2023_8/lib/advent_of_code20238.ex
@@ -11,34 +11,71 @@ defmodule AdventOfCode20238 do
     end
   end
 
-  def follow_instructions(node, instructions, %{} = nodes, len, ended?) when is_binary(node) do
-    if ended?.(node) do
-      len
-    else
-      instruction = elem(instructions, rem(len, tuple_size(instructions)))
-      follow_instructions(nodes[node][instruction], instructions, nodes, len + 1, ended?)
-    end
+  def next_node("L", %{root: node_root}, nodes) do
+    next_root =
+      nodes
+      |> Enum.find(&(&1.root == node_root))
+      |> then(& &1.left)
+
+    Enum.find(nodes, &(&1.root == next_root))
+  end
+
+  def next_node("R", %{root: node_root}, nodes) do
+    next_root =
+      nodes
+      |> Enum.find(&(&1.root == node_root))
+      |> then(& &1.right)
+
+    Enum.find(nodes, &(&1.root == next_root))
+  end
+
+  def next_instruction(instructions, ind) when (ind + 1) in 0..(length(instructions) - 1)//1,
+    do: Enum.at(instructions, ind + 1)
+
+  def next_instruction(instructions, invalid_ind),
+    do: next_instruction(instructions, invalid_ind - length(instructions))
+
+  def count_steps(%{root: "ZZZ"}, _nodes, {_instruction, instruction_ind}, _instructions),
+    do: instruction_ind
+
+  def count_steps(node, nodes, {instruction, instruction_ind}, instructions) do
+    next_node = next_node(instruction, node, nodes)
+    next_instruction = next_instruction(instructions, instruction_ind)
+
+    count_steps(next_node, nodes, {next_instruction, instruction_ind + 1}, instructions)
   end
 end
 
 [raw_instructions, _ | raw_nodes] = AdventOfCode20238.read_file(:real)
 
-instructions =
-  raw_instructions
-  |> String.to_charlist()
-  |> List.to_tuple()
+instructions = String.graphemes(raw_instructions)
 
 nodes =
-  Enum.map(raw_nodes, fn node ->
-    <<root::binary-size(3), " = (", left::binary-size(3), ", ", right::binary-size(3), ")">> =
-      node
+  raw_nodes
+  |> Enum.map(&String.split(&1, " = "))
+  |> Enum.map(fn [root, raw_branches] ->
+    [left, right] =
+      raw_branches
+      |> String.split(", ")
+      |> Enum.map(&Regex.replace(~r/[^A-Z]/, &1, ""))
 
-    {root, %{?L => left, ?R => right}}
+    %{root: root, left: left, right: right}
   end)
-  |> Enum.into(%{})
 
-data =
-  %{instructions: instructions, nodes: nodes}
+initial_node =
+  nodes
+  |> Enum.find(&(&1.root == "AAA"))
 
-AdventOfCode20238.follow_instructions("AAA", data.instructions, data.nodes, 0, &(&1 == "ZZZ"))
+AdventOfCode20238.count_steps(
+  initial_node,
+  nodes,
+  instructions |> Enum.with_index() |> Enum.at(0),
+  instructions
+)
+
+# instructions
+# |> Enum.with_index()
+# |> Enum.reduce_while(Enum.at(nodes, 0), fn {instruction, instruction_ind}, acc ->
+# {:halt, acc}
+# end)
 |> IO.inspect()
