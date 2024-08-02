07274c4ffeab9282fee78ee096308531722e3d8b
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-08-02 17:47:50 -0300

    implementig aoc 2022 day 7 task 1

--- advent_of_code_2022_7/lib/advent_of_code20227.ex
+++ advent_of_code_2022_7/lib/advent_of_code20227.ex
@@ -12,113 +12,91 @@ defmodule AdventOfCode20227 do
     end
   end
 
-  def typify_lines(lines) do
+  def prepare_lines(lines) do
     lines
-    |> Enum.map(fn line ->
+    |> Enum.with_index()
+    |> Enum.map(fn {line, line_id} ->
       cond do
-        String.starts_with?(line, "$") -> %{value: line, type: :command}
-        String.starts_with?(line, "dir") -> %{value: line, type: :directory}
-        true -> %{value: line, type: :file}
+        String.starts_with?(line, "$") -> %{value: line, type: :command, line_id: line_id}
+        String.starts_with?(line, "dir") -> %{value: line, type: :directory, line_id: line_id}
+        true -> %{value: line, type: :file, line_id: line_id}
       end
     end)
   end
 
-  def get_total_size(files) do
-    files
-    |> Enum.reduce(0, fn %{value: value}, acc ->
-      [size, _name] = String.split(value)
+  def interpret_line(%{type: :command, value: "$ cd .."} = _line, %{curr_dir: curr_dir} = state) do
+    state
+    |> Map.put(:curr_dir, tl(curr_dir))
+  end
 
-      acc + String.to_integer(size)
+  def interpret_line(
+        %{type: :command, value: <<"$ cd ", cd_into::binary>>},
+        %{curr_dir: curr_dir} = state
+      ) do
+    state
+    |> Map.put(:curr_dir, [cd_into | curr_dir])
+  end
+
+  def interpret_line(%{type: :command, value: "$ ls"}, state) do
+    state
+  end
+
+  def interpret_line(
+        %{type: :directory, value: <<"dir ", _dir_name::binary>>},
+        state
+      ) do
+    state
+  end
+
+  def interpret_line(
+        %{type: :file, value: file},
+        %{curr_dir: curr_dir, dir_sizes: dir_sizes} = state
+      ) do
+    [file_size, _file_name] = String.split(file)
+
+    curr_dir_size = Map.get(dir_sizes, curr_dir)
+
+    new_dir_size =
+      case curr_dir_size do
+        nil -> String.to_integer(file_size)
+        _ -> Map.get(dir_sizes, curr_dir) + String.to_integer(file_size)
+      end
+
+    updated_dir_sizes =
+      dir_sizes
+      |> Map.put(curr_dir, new_dir_size)
+
+    state
+    |> Map.put(:dir_sizes, updated_dir_sizes)
+  end
+
+  def total_size_of_dir(%{dir_sizes: dir_sizes} = _state, dir_name) do
+    dir_sizes
+    |> Enum.reduce(0, fn {path, size}, total ->
+      cond do
+        Enum.all?(dir_name, &(&1 in path)) -> total + size
+        true -> total
+      end
     end)
-  end
-
-  defp run_command(%{value: "$ cd .."}, state) do
-    Map.put(state, :current_directory, tl(state.current_directory))
-  end
-
-  defp run_command(%{value: <<"$ cd ", dir_name::binary>>}, state) do
-    if Regex.match?(~r/[a-z\/]/, dir_name),
-      do:
-        Map.put(state, :directories, [
-          %{name: dir_name, path: state.current_directory} | state.directories
-        ])
-        |> Map.put(:current_directory, [dir_name | state.current_directory]),
-      else: state
-  end
-
-  defp run_command(%{value: "$ ls"}, state) do
-    typed_lines =
-      AdventOfCode20227.read_file()
-      |> AdventOfCode20227.typify_lines()
-
-    files =
-      typed_lines
-      |> Enum.drop_while(&(&1.value != "$ cd #{hd(state.current_directory)}"))
-      |> Enum.drop(2)
-      |> Enum.take_while(fn
-        %{value: <<"$ cd ", dir_name::binary>>} ->
-          not Regex.match?(~r/[a-z\/]/, dir_name)
-
-        _ ->
-          true
-      end)
-      |> Enum.reject(&(&1.type != :file))
-
-    directory_with_files =
-      Map.get(state, :directories)
-      |> Enum.find(&(&1.name == hd(state.current_directory)))
-      |> Map.update(:files, files, fn _ -> files end)
-
-    Map.get(state, :directories)
-    |> Enum.reject(&(&1.name == hd(state.current_directory)))
-    |> then(fn directories ->
-      [directory_with_files | directories]
-    end)
-    |> then(fn updated_directories ->
-      Map.put(state, :directories, updated_directories)
-    end)
-  end
-
-  defp run_command(_, state), do: state
-
-  def get_tree() do
-    AdventOfCode20227.read_file()
-    |> AdventOfCode20227.typify_lines()
-    |> Enum.filter(&(&1.type == :command))
-    |> Enum.reduce(
-      %{
-        directories: [],
-        current_directory: []
-      },
-      &run_command/2
-    )
-  end
-
-  def get_files_of_directory(tree, dir_name) do
-    tree
-    |> Map.get(:directories)
-    |> Enum.filter(&(dir_name in &1.path or &1.name == dir_name))
-    |> Enum.reduce([], fn files, acc -> [files | acc] end)
   end
 end
 
-AdventOfCode20227.get_tree()
-|> Map.get(:directories)
-|> Enum.map(fn %{name: dir_name} ->
-  AdventOfCode20227.get_tree()
-  |> AdventOfCode20227.get_files_of_directory(dir_name)
-  |> Enum.map(fn %{files: files} ->
-    files
-    |> Enum.map(fn %{value: value} ->
-      [size, _name] = String.split(value)
-
-      String.to_integer(size)
-    end)
-    |> Enum.sum()
+AdventOfCode20227.read_file(:example_2)
+|> AdventOfCode20227.prepare_lines()
+|> Enum.reduce(
+  %{
+    curr_dir: [],
+    dir_sizes: %{}
+  },
+  fn line, state ->
+    AdventOfCode20227.interpret_line(line, state)
+  end
+)
+|> then(fn %{dir_sizes: dir_sizes} = state ->
+  dir_sizes
+  |> Enum.reduce(0, fn {dir_name, _dir_size} = _dir_info, total ->
+    size = AdventOfCode20227.total_size_of_dir(state, dir_name)
+    if size <= 100_000, do: total + size, else: total
   end)
-  |> Enum.sum()
-  |> IO.inspect(label: dir_name)
 end)
-|> Enum.reject(&(&1 > 100_000))
-|> Enum.sum()
-|> IO.inspect()
+|> IO.inspect(label: "task 1")
