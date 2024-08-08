53d1c6765a3e968202cae6e51ebbea095fb286e0
caioalmeida12 <araujo.caio08@aluno.ifce.edu.br>
2024-08-08 13:21:47 -0300

    removed old code on k nearest neighbours

--- k_nearest_neighbours/lib/k_nearest_neighbours.ex
+++ k_nearest_neighbours/lib/k_nearest_neighbours.ex
@@ -38,38 +38,6 @@ defmodule KNearestNeighbours do
     |> Enum.take_every(1)
   end
 
-  def numbers_matrix_to_points_matrix(numbers_matrix) do
-    numbers_matrix
-    |> Enum.with_index(fn line, line_ind ->
-      line
-      |> Enum.with_index(fn val, col_ind ->
-        %{
-          value: val,
-          pos: {line_ind, col_ind},
-          class: if(Integer.is_even(val), do: :even, else: :odd)
-        }
-      end)
-    end)
-  end
-
-  def neighbours(points_matrix, %{pos: {ref_line, ref_col}} = reference, k_value) do
-    min_line = max(ref_line - k_value, 0)
-    max_line = min(ref_line + k_value, length(hd(points_matrix)))
-    min_col = max(ref_col - k_value, 0)
-    max_col = min(ref_line + k_value, length(hd(points_matrix)))
-
-    points_matrix
-    |> List.flatten()
-    |> Enum.reject(&(&1.pos == reference.pos))
-    |> Enum.reduce([], fn %{pos: {dest_line, dest_col}} = destination, acc ->
-      if(dest_line in min_line..max_line and dest_col in min_col..max_col) do
-        [destination | acc]
-      else
-        acc
-      end
-    end)
-  end
-
   def neighbours_distances(points_matrix, %{dimensions: ref_dimensions} = _reference) do
     distances =
       points_matrix
@@ -88,14 +56,6 @@ defmodule KNearestNeighbours do
       Map.put(point, :distance, distance)
     end)
     |> Enum.reject(&(&1.distance == 0))
-  end
-
-  def distance(
-        %{pos: {ref_line, ref_col}} = _reference,
-        %{pos: {dest_line, dest_col}} = _destination
-      ) do
-    (:math.pow(ref_line - dest_line, 2) + :math.pow(ref_col - dest_col, 2))
-    |> then(&:math.sqrt/1)
   end
 
   def predict(points_matrix, reference, k_value) do
