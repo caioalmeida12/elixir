396fd9d33274a40ca0b59016788c97a7a7d67eb9
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-09-06 09:13:34 -0300

    temp: working on aoc 2023 9

--- advent_of_code_2023_9/lib/advent_of_code20239.ex
+++ advent_of_code_2023_9/lib/advent_of_code20239.ex
@@ -15,54 +15,39 @@ defmodule AdventOfCode20239 do
   def format_input(raw_input) do
     raw_input
     |> Enum.with_index()
-    |> Enum.map(fn {line, layer} ->
-      line
-      |> String.split()
-      |> Enum.map(&String.to_integer/1)
+    |> Enum.map(fn {line_number, line_content} ->
+      {
+        line_content,
+        line_number
+        |> String.split()
+        |> Enum.map(&String.to_integer/1)
+      }
     end)
   end
 
-  def get_difference_for_layer(single_layer) do
-    single_layer
-    |> Enum.with_index()
-    |> Enum.flat_map(fn {curr, ind} ->
-      next = Enum.at(single_layer, ind + 1)
+  def differentiate_line({_line_number, line_content}),
+    do: differentiate_line(line_content, %{}, 0)
 
-      if is_integer(next), do: [next - curr], else: []
-    end)
-  end
-
-  def get_layered_differences(single_layer) do
-    layered_differences =
-      get_difference_for_layer(single_layer)
-
-    if(Enum.any?(layered_differences, &(&1 > 0))) do
-      [single_layer, layered_differences, get_difference_for_layer(layered_differences)]
-    else
-      IO.inspect([single_layer, layered_differences], label: "stop")
-      [single_layer, layered_differences]
-    end
-  end
-
-  def extrapolate(all_layers) do
-    number_of_layers = Enum.count(all_layers)
-
-    all_layers
-    |> Enum.reverse()
-    |> Enum.reduce(Enum.at(all_layers, -2) |> Enum.at(-1), fn layer, acc ->
-      IO.inspect(layer, label: "current layer")
-      IO.inspect(Enum.at(layer, -1), label: "last digit of layer")
-      IO.inspect(acc, label: "acc")
-      Enum.at(layer, -1) + acc
+  def differentiate_line(line_content, acc, key) when is_map(acc) do
+    acc
+    |> Map.put_new(
+      key,
+      line_content
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.map(fn [a, b] -> b - a end)
+    )
+    |> then(fn new_acc ->
+      if Enum.sum(Map.get(new_acc, key)) == 0 do
+        differentiate_line(line_content, new_acc, key + 1)
+      else
+        new_acc
+      end
     end)
   end
 end
 
 AdventOfCode20239.read_file()
 |> AdventOfCode20239.format_input()
-|> Enum.map(fn history ->
-  AdventOfCode20239.get_layered_differences(history)
-
-  # |> AdventOfCode20239.extrapolate()
-end)
+|> Enum.at(0)
+|> AdventOfCode20239.differentiate_line()
 |> IO.inspect(charlists: :as_lists, label: "task  1")
