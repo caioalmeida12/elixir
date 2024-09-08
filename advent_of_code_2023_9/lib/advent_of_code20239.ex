2508263fed56775baf73d4844e0455ea2d480ff9
caioalmeida12 <araujo.caoi08@aluno.ifce.edu.br>
2024-09-08 17:02:32 -0300

    feat: implemented differentiate_line in aoc 2023 day 9 task 1

--- advent_of_code_2023_9/lib/advent_of_code20239.ex
+++ advent_of_code_2023_9/lib/advent_of_code20239.ex
@@ -14,40 +14,35 @@ defmodule AdventOfCode20239 do
 
   def format_input(raw_input) do
     raw_input
-    |> Enum.with_index()
-    |> Enum.map(fn {line_number, line_content} ->
-      {
-        line_content,
-        line_number
-        |> String.split()
-        |> Enum.map(&String.to_integer/1)
-      }
+    |> Enum.map(fn line_number ->
+      line_number
+      |> String.split()
+      |> Enum.map(&String.to_integer/1)
     end)
   end
 
-  def differentiate_line({_line_number, line_content}),
-    do: differentiate_line(line_content, %{}, 0)
+  def differentiate_line(line_content),
+    do: differentiate_line(%{0 => line_content}, 0)
 
-  def differentiate_line(line_content, acc, key) when is_map(acc) do
-    acc
-    |> Map.put_new(
-      key,
-      line_content
-      |> Enum.chunk_every(2, 1, :discard)
-      |> Enum.map(fn [a, b] -> b - a end)
-    )
-    |> then(fn new_acc ->
-      if Enum.sum(Map.get(new_acc, key)) == 0 do
-        differentiate_line(line_content, new_acc, key + 1)
-      else
-        new_acc
-      end
-    end)
+  def differentiate_line(acc, current_layer_index) when is_map(acc) do
+    new_acc =
+      acc
+      |> Map.put_new(
+        current_layer_index + 1,
+        Map.get(acc, current_layer_index)
+        |> Enum.chunk_every(2, 1, :discard)
+        |> Enum.map(fn [a, b] -> b - a end)
+      )
+
+    new_line_content = Map.get(new_acc, current_layer_index + 1)
+
+    if Enum.sum(new_line_content) == 0,
+      do: new_acc,
+      else: differentiate_line(new_acc, current_layer_index + 1)
   end
 end
 
 AdventOfCode20239.read_file()
 |> AdventOfCode20239.format_input()
-|> Enum.at(0)
-|> AdventOfCode20239.differentiate_line()
+|> Enum.map(&AdventOfCode20239.differentiate_line/1)
 |> IO.inspect(charlists: :as_lists, label: "task  1")
