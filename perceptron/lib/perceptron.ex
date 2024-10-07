b11a273823da64e3e89c0c2f3538248242cd9866
caioalmeida12 <araujo.caio08@aluno.ifce.edu.br>
2024-10-07 19:44:48 -0300

    working on perceptron

--- perceptron/lib/perceptron.ex
+++ perceptron/lib/perceptron.ex
@@ -1,13 +1,8 @@
 defmodule Perceptron do
   defstruct bias: 0, weights: []
 
-  def evaluate(all_inputs, all_weights, bias) when is_list(all_inputs) and is_list(all_weights) and is_number(bias) do
-    Enum.zip(all_inputs, all_weights)
-    |> Enum.reduce(bias, fn {inputs, weights}, sum ->
-      Enum.zip(inputs, weights)
-      sum + Neuron.activate(inputs, weights)
-    end)
-    |> then(fn result -> if result >= 0, do: 1, else: 0 end)
+  def evaluate(perceptron, inputs, bias) when is_map(perceptron) and is_list(inputs) do
+
   end
 end
 
